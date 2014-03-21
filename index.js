9256536fe03f7cdebdb5b3595afde4b0ce6d49b1
Alexey Novikov <velocityzen@gmail.com>
2014-03-21 15:14:11 +0400

    Updated to latest fuller api

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 "use strict";
 var fs = require('fs');
 var path = require('path');
+var UglifyJS = require('uglify-js');
+var compressor = UglifyJS.Compressor();
 
-var Stream = require('./stream');
+var FILE_ENCODING = 'utf-8';
 
-var Uglify = function(fuller, plan) {
-	this.options = plan.defaults.stream || {};
-	this.options.compress = !plan.defaults.dev;
+var Uglify = function(fuller, options) {
+	this.Stream = fuller.streams.Capacitor;
+	this.compress = !options.dev;
+	this.src = options.src;
+	this.dst = options.dst;
 };
 
-Uglify.prototype.getStream = function() {
-	return new Stream(this.options);
+Uglify.prototype.build = function(stream, master) {
+	var self = this,
+		next = new this.Stream(this.compress, function(result, cb) {
+			self.uglify(result, cb);
+		});
+
+	if(typeof stream === "string") {
+		var src = path.join(this.src, stream);
+		this.addDependence(src, master);
+		return fs.createReadStream(src, {encoding: FILE_ENCODING}).pipe(next);
+	} else {
+		return stream.pipe(next);
+	}
+};
+
+Uglify.prototype.uglify = function(jsString, cb) {
+	var ast;
+
+	try {
+		ast = UglifyJS.parse(jsString);
+	} catch (err) {
+		cb(err);
+		process.exit();
+	}
+
+	ast.figure_out_scope();
+	ast = ast.transform(compressor);
+	ast.figure_out_scope();
+	ast.compute_char_frequency();
+	ast.mangle_names();
+
+	cb(null, ast.print_to_string());
 };
 
 module.exports = Uglify;
