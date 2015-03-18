12700cc3b03a5f50c76664a77abc69da5f13092b
Alexey Novikov <velocityzen@gmail.com>
2015-03-18 16:22:27 -0400

    new tool api

--- index.js
+++ index.js
@@ -1,57 +1,28 @@
 "use strict";
-let path = require("path");
-let through2 = require("through2");
 let uglifyjs = require("uglify-js");
 let compressor = uglifyjs.Compressor();
 
-let Uglify = function(fuller, options) {
-	fuller.bind(this);
-
-	this.compress = !options.dev;
-	this.dst = options.dst;
-};
-
-Uglify.prototype.compile = function(jsString, master, cb) {
-	try {
-		let ast = uglifyjs.parse(jsString);
-		ast.figure_out_scope();
-		ast = ast.transform(compressor);
-		ast.figure_out_scope();
-		ast.compute_char_frequency();
-		ast.mangle_names();
-
-		cb(null, ast.print_to_string());
-	} catch (err) {
-		this.error({
-			message: err.message,
-			line: err.line,
-			column: err.col,
-			file: path.join(this.dst, master)
+module.exports = function(f, mat, options, next) {
+	if(options.dev) {
+		next(null, mat);
+	} else {
+		mat.getContent(function(content) {
+			try {
+				let ast = uglifyjs.parse(content.toString());
+				ast.figure_out_scope();
+				ast = ast.transform(compressor);
+				ast.figure_out_scope();
+				ast.compute_char_frequency();
+				ast.mangle_names();
+				next(null, mat.setContent(ast.print_to_string()));
+			} catch (err) {
+				next({
+					message: err.message,
+					line: err.line,
+					column: err.col,
+					file: mat.dst().path
+				});
+			}
 		});
-		cb();
 	}
 };
-
-Uglify.prototype.build = function(stream, master) {
-	if(!this.compress) { return stream;}
-
-	let self = this,
-		buffer = [];
-
-	return stream.pipe( through2(
-		function(chunk, enc, cb) {
-			buffer.push(chunk);
-			cb();
-		},
-		function(cb) {
-			let that = this;
-			self.compile(buffer.join(""), master, function(err, result) {
-				!err && that.push(result);
-				cb();
-			});
-		}
-	));
-};
-
-
-module.exports = Uglify;
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # Uglify js tool for Fuller #
 **Build everything with right tool**
+
 [For more info about fuller](https://github.com/fullerjs/fuller)
 
 *In fuller's dev mode (-z option) outputs untouched js.*
