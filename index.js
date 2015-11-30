a2f3e98e0b0525518080cbabe45dd36857d8b09f
velocityzen <velocityzen@gmail.com>
2015-11-30 15:25:05 -0500

    Updated Uglify-js to 2.6.0. Added editor config

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-"use strict";
-let uglifyjs = require("uglify-js");
+'use strict';
+let uglifyjs = require('uglify-js');
 let compressor = uglifyjs.Compressor();
 
 module.exports = function(f, mat, options, next) {
-	if(options.dev) {
-		next(null, mat);
-	} else {
-		mat.getContent(function(content) {
-			try {
-				let ast = uglifyjs.parse(content.toString());
-				ast.figure_out_scope();
-				ast = ast.transform(compressor);
-				ast.figure_out_scope();
-				ast.compute_char_frequency();
-				ast.mangle_names();
-				next(null, mat.setContent(ast.print_to_string()));
-			} catch (err) {
-				next({
-					message: err.message,
-					line: err.line,
-					column: err.col,
-					file: mat.dst().path
-				});
-			}
-		});
-	}
+  if (options.dev) {
+    next(null, mat);
+  } else {
+    mat.getContent(function(content) {
+      try {
+        let ast = uglifyjs.parse(content.toString());
+        ast.figure_out_scope();
+        ast = ast.transform(compressor);
+        ast.figure_out_scope();
+        ast.compute_char_frequency();
+        ast.mangle_names();
+        next(null, mat.setContent(ast.print_to_string()));
+      } catch (err) {
+        next({
+          message: err.message,
+          line: err.line,
+          column: err.col,
+          file: mat.dst().path
+        });
+      }
+    });
+  }
 };
