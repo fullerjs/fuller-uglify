80ea82dd58c043d4d7e157952afa63f208285675
velocityzen <velocityzen@gmail.com>
2018-11-07 13:52:49 -0500

    Updated to 0.4 api

--- index.js
+++ index.js
@@ -2,28 +2,29 @@
 const uglify = require('uglify-es');
 
 module.exports = function(f, mat, options, next) {
-  if (options.dev) {
+  if (!options.uglify) {
     next(null, mat);
-  } else {
-    mat.getContent( content => {
-      const opts = options.uglify || {};
-      const res = uglify.minify(content.toString(), opts);
-
-      if (res.error) {
-        const err = res.error;
-        return next({
-          message: err.message,
-          line: err.line,
-          column: err.col,
-          file: mat.dst().path
-        });
-      }
-
-      if (opts.warnings && res.warnings) {
-        res.warnings.forEach(warn => f.log(warn));
-      }
-
-      next(null, mat.setContent(res.code));
-    });
+    return;
   }
+
+  mat.getContent(content => {
+    const opts = options.uglify || {};
+    const res = uglify.minify(content.toString(), opts);
+
+    if (res.error) {
+      const err = res.error;
+      return next({
+        message: err.message,
+        line: err.line,
+        column: err.col,
+        file: mat.dst().path
+      });
+    }
+
+    if (opts.warnings && res.warnings) {
+      res.warnings.forEach(warn => f.log(warn));
+    }
+
+    next(null, mat.setContent(res.code));
+  });
 };
